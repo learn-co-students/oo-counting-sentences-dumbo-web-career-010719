d18b5cbde8f9259973df76420839f9a4ca1217ec
rchow1944 <rchow2@buffalo.edu>
2019-01-09 16:49:50 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split.map {|str| str.sentence? || str.question? || str.exclamation?}.count {|val| val == true}
   end
 end
