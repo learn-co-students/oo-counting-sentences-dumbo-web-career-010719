ec0cf18b9461e992dc76a59e6a406a77382f4e85
github username <github email address>
2019-01-11 12:09:25 -0500

    Tests passed.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #binding.pry
+    #count = self.split.length
+    #count
+    #binding.pry
+     self.split(/\.|\?|!/).delete_if do |word|
+       word.length < 2
+     end.length
+    # binding.pry
   end
 end
