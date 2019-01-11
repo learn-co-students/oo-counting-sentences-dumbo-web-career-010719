7bb426d98424890ceb7e94f7d7e7b60100c26ee4
Daniel Toma <danieltoma@ubalumni.com>
2019-01-11 19:58:03 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,12 +16,19 @@ class String
   end
 
   def count_sentences
+    sentence_counter = 0
     sentence_array = self.split(' ')
-    puts sentence_array
+    sentence_array.each do |sentence|
+      if sentence.end_with?(".") || sentence.end_with?("?") || sentence.end_with("!")
+         sentence_counter += 1
+      end
+    end
     # index = 0
     # while index < self.length
     #   puts self[index]
     #   index += 1
     # end
+    return sentence_counter
   end
+
 end
